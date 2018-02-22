e27e624ddfe71cbe7a65ff82d2988df250bf7ea4
Logan Lasiter <lasiterlogan@gmail.com>
2018-02-21 18:25:06 -0800

    decided to not go with a tutorial, setup server with test route.

--- crypto-backend/app.py
+++ crypto-backend/app.py
@@ -1,19 +1,17 @@
-# users-service/project/__init__.py
-
-
+import os
 from flask import Flask, jsonify
-
 
 # instantiate the app
 app = Flask(__name__)
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
-
-
-@app.route('/users/ping', methods=['GET'])
+@app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
         'status': 'success',
         'message': 'pong!'
     })
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
